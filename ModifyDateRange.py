f100a902178cc029c5de79db216d69fa819568d1
Darcy Ian Trenfield <ditre3@student.monash.edu>
2018-09-03 12:32:00 +1000

    Upload New File

---
+++ ModifyDateRange.py
@@ -0,0 +1,71 @@
+#Opens data file 
+Sprintdoc = open('Sprintdoc.txt')
+
+#Global variables 
+finder = 'modifiedDate'
+dateList = []
+
+def formatDateRange(dateRange):
+    dateRangeList = []
+    dateRangeList.append(dateRange[:10])
+    dateRangeList.append(dateRange[11:])
+    return dateRangeList
+                         
+
+#Strips target line of unnecessary characters 
+def formatDate(line, finder):
+    line = line.replace('"', '')
+    line = line.replace(':', '', 1)
+    line = line.replace(',', '')
+    line = line.replace(finder, '')
+    line = line.strip()
+    line = line[:10]
+    return line
+
+#Checks if user name already in docNames list
+def checkDate(dateList, line):
+    if len(dateList)>0:
+        for i in range(len(dateList)):
+            if line in dateList[i]:
+                return False
+        return True
+    else:
+        return True
+
+def withinRange(dateRangeList, line):
+    if line>=dateRangeList[0] and line<=dateRangeList[1]:
+        return True
+    else:
+        return False
+    
+#checks data file for user names and adds them to docNames list 
+def findDate(dateRangeList):
+    for line in Sprintdoc:
+        if finder in line:
+            line = ''.join(line)
+            line = formatDate(line, finder)
+            if withinRange(dateRangeList, line) == True:
+                if checkDate(dateList, line) == True:
+                    dateList.append(line)
+
+#Prints user names 
+def printdateList(dateList):
+    print('\nDates of document edit between specified range are:\n')
+    for i in range(len(dateList)):
+        print(dateList[i])
+            
+
+
+dateRange = input("Enter date range in format yyyy-mm-dd/yyyy-mm-dd:\n")
+dateRangeList = formatDateRange(dateRange)
+findDate(dateRangeList)
+
+printdateList(dateList)
+
+
+            
+            
+            
+            
+    
+     
