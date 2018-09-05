95238444cedd61bdae68324af7570e2169a33e1e
Quinn Roberts <quinn@jelly-snakes.com>
2018-09-05 18:55:11 +1000

    Add option to specify granularity for the revisions

--- protoyping/quick.py
+++ protoyping/quick.py
@@ -235,58 +235,6 @@ class RevisionMetadata:
         return self.change
 
 
-class UnsafeRequester:
-    """
-    Makes requests to the unsafe API
-    """
-
-    def __init__(self, http, docId):
-        """
-
-        :param http: The http object to make the direct calls with
-        :param docId: The ID of the document to call against.
-        """
-        self.http = http
-        self.docId = docId
-        self.baseUrl = f"https://docs.google.com/document/d/{self.docId}/"
-
-    def requestRevision(self, revision):
-        """
-        Requests the detailed revision data for a revision
-
-        :param revision: The revision to request for
-        :return: The raw json of that revision
-        """
-        return self.requestRevisionRange(revision.startId, revision.endId)
-
-    def requestRevisionRange(self, startId, endId):
-        """
-        Requests the detailed revision data for a given revision range.
-
-        :param startId: The start id of the range
-        :param endId: The end id of the range
-        :return: The raw json for that range, as a single revision object.
-        """
-        (_, content) = self.http.request(
-            self.baseUrl +
-            f"showrevision?id={self.docId}&"
-            f"end={endId}&start={startId}")
-        return json.loads(content[5:])
-
-    def requestList(self):
-        """
-        Request a list of all revisions on this document
-
-        :return: The raw json of the list of revisions
-        """
-        (_, content) = self.http.request(self.baseUrl +
-                                         f"revisions/tiles?id={self.docId}&"
-                                         f"start=1&"
-                                         f"showDetailedRevisions=false&filterNamed=false")
-
-        return json.loads(content[5:])
-
-
 class User:
     """
     Represents a single user.
@@ -318,14 +266,14 @@ class Document:
     This is made up of multiple revisions.
     """
 
-    def __init__(self, http, docId):
+    def __init__(self, http, docId, useFine=False):
         """
         Creates a new document with the given id.
 
         :param http: The http object to make calls with
         :param docId: The ID of the document in question
         """
-        self.requester = UnsafeRequester(http, docId)
+        self.requester = UnsafeRequester(http, docId, useFine)
         self.docId = docId
         self.revisions = None
         self.users = None
@@ -434,6 +382,60 @@ class Document:
             if revisions[i].startId >= startId and revisions[i].endId <= endId:
                 changes.mergeIn(revisions[i].getChanges())
         return changes
+
+
+class UnsafeRequester:
+    """
+    Makes requests to the unsafe API
+    """
+
+    def __init__(self, http, docId, useFine=False):
+        """
+
+        :param http: The http object to make the direct calls with
+        :param docId: The ID of the document to call against.
+        """
+        self.http = http
+        self.docId = docId
+        self.useFine = useFine
+        self.baseUrl = f"https://docs.google.com/document/d/{self.docId}/"
+
+    def requestRevision(self, revision):
+        """
+        Requests the detailed revision data for a revision
+
+        :param revision: The revision to request for
+        :return: The raw json of that revision
+        """
+        return self.requestRevisionRange(revision.startId, revision.endId)
+
+    def requestRevisionRange(self, startId, endId):
+        """
+        Requests the detailed revision data for a given revision range.
+
+        :param startId: The start id of the range
+        :param endId: The end id of the range
+        :return: The raw json for that range, as a single revision object.
+        """
+        (_, content) = self.http.request(
+            self.baseUrl +
+            f"showrevision?id={self.docId}&"
+            f"end={endId}&start={startId}")
+        return json.loads(content[5:])
+
+    def requestList(self):
+        """
+        Request a list of all revisions on this document
+
+        :return: The raw json of the list of revisions
+        """
+        (_, content) = self.http.request(self.baseUrl +
+                                         f"revisions/tiles?id={self.docId}&"
+                                         f"start=1&"
+                                         f"showDetailedRevisions={'true' if self.useFine else 'false'}"
+                                         f"&filterNamed=false")
+
+        return json.loads(content[5:])
 
 
 def main():
