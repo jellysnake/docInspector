d56d6bd40889e7f83509453bc22659b44ff69898
honeybadger26 <honeybadger26@gmail.com>
2018-09-06 21:59:55 +1000

    -improved 'create_timeline' function

--- timeline.py
+++ timeline.py
@@ -2,19 +2,31 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+from datetime import datetime
+
 
 # If modifying these scopes, delete the file token.json
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+
 
 ''' 
 Prints timeline of given doc to the console
 :parameter rev_meta : revision metadata from google docs API
 '''
 def create_timeline(rev_meta):
+    file = open('file_timeline.txt', 'w')
     revisions = rev_meta.get('items', [])
+
     for revision in revisions:
-        print('\t {0}/{1}/{2} {3} - {4}'.format(revision['modifiedDate'][8:10], revision['modifiedDate'][5:7],
-            revision['modifiedDate'][2:4], revision['modifiedDate'][11:16], revision['lastModifyingUserName']))
+        date_time = datetime.strptime(revision['modifiedDate'][2:18], "%y-%m-%dT%H:%M:%S").strftime("%d/%m/%y %I:%M:%S %p")
+
+        file.write('{0} - {1}\n'.format(
+            date_time,
+            revision['lastModifyingUserName']
+        ))
+
+    file.close()
+
 
 if __name__ == '__main__':
     # Authentication
@@ -26,9 +38,9 @@ if __name__ == '__main__':
     service = build('drive', 'v2', http=creds.authorize(Http()))
 
     # Print file name
-    doc_id = '1lt2SmAjRs14EqwlKyix8VsypZE0mBSfRztkjzma0BjI'
-    file_meta = service.files().get(fileId=doc_id).execute()
-    print(file_meta.get('title'))
+    doc_id = '17kB9r4NG2akVqVE6-FmLP9xT6mKhoI5AKPkO4dhRAxo'
+    # file_meta = service.files().get(fileId=doc_id).execute()
+    # print(file_meta.get('title'))
 
     # Print revision dates and last modified user
     rev_meta = service.revisions().list(fileId=doc_id).execute()
