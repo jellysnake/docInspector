f3b8413c526d02084d815b5378555629809601bd
Quinn Roberts <quinn@jelly-snakes.com>
2018-09-10 17:58:08 +1000

    docstrings for methods and classes

--- DocStats.py
+++ DocStats.py
@@ -2,16 +2,34 @@ from typing import List
 
 
 class TimelineStats:
+    """
+    Collates stats calculated for the timeline
+    """
+
     class IncrementStats:
+        """
+        Represents a single increment of time
+        """
+
         def __init__(self):
             self.additions = {}
             self.removals = {}
             self.changes = {}
 
         def isEmpty(self):
+            """
+            :return: True if the increment contained no changes. False otherwise
+            """
             return self.changes
 
         def addAddition(self, editor, size):
+            """
+            Adds an addition to the increment.
+            Automatically updates the edit counter.
+
+            :param editor: The id of the editor that made the addition
+            :param size: The size of the edit in characters
+            """
             if editor not in self.additions:
                 self.additions[editor] = 0
                 self.changes[editor] = 0
@@ -20,6 +38,13 @@ class TimelineStats:
             self.changes[editor] += 1
 
         def addRemoval(self, editor, size):
+            """
+            Adds a removal to the increment.
+            Automatically updates the edit counter.
+
+            :param editor: The editor that made the removal
+            :param size: The size of the edit in characters
+            """
             if editor not in self.additions:
                 self.removals[editor] = 0
                 self.changes[editor] = 0
@@ -32,27 +57,60 @@ class TimelineStats:
         self.incrementSize = size
 
     def getIncrement(self, id) -> IncrementStats:
+        """
+        Get the given increment.
+
+        :param id: The id of the increment to get
+        :return: The increment, if it exists.
+        """
         return self.increments[id]
 
     def makeIncrement(self) -> IncrementStats:
+        """
+        Appends a new increment and adds it to the list
+
+        :return: THe newly created increment
+        """
         self.increments.append(self.IncrementStats())
         return self.increments[-1]
 
     def removeIncrement(self, id):
+        """
+        Removes the given increment from the list
+
+        :param id: The increment to remove
+        """
         self.increments.pop(id)
 
     def getIncrementSize(self):
+        """
+        :return: The size of each increment
+        """
         return self.incrementSize
 
     def setIncrementSize(self, size):
+        """
+        :param size:  the size of each increment
+        """
         self.incrementSize = size
 
     def getNumIncrements(self):
+        """
+        :return: how many increments are stored
+        """
         return len(self.increments)
 
 
 class IndividualStats:
+    """
+    Collates all the stats for the individual data section
+    """
+
     class EditorStats:
+        """
+        Represents stats made by an individual editor
+        """
+
         def __init__(self):
             self.additions = 0
             self.removals = 0
@@ -61,10 +119,22 @@ class IndividualStats:
             self.percent = 0
 
         def addAddition(self, size):
+            """
+            Adds an addition to the editor.
+            Automatically increases the edit counter
+
+            :param size: The size of the addition in characters
+            """
             self.additions = size
             self.changes += 1
 
         def addRemoval(self, size):
+            """
+            Adds an removal to the editor.
+            Automatically increases the edit counter
+
+            :param size: The size of the removal in characters
+            """
             self.removals = size
             self.changes += 1
 
@@ -72,24 +142,46 @@ class IndividualStats:
         self.editors = {}
 
     def getEditor(self, id) -> EditorStats:
+        """
+        Gets an editor's contributions
+        :param id: The id of the editor to get
+        :return: An EditorStats of the edits they made
+        """
         return self.editors[id]
 
     def removeEditor(self, id):
+        """
+        Removes an editor from the list
+
+        :param id: The id of the editor to remove
+        """
         del self.editors[id]
 
     def makeEditor(self, id) -> EditorStats:
+        """
+        Makes a new editor and adds it to the list.
+        Returns the newly made editor
+
+        :param id: The id of the editor just made
+        """
         self.editors[id] = self.EditorStats()
         return self.editors[id]
 
     def getEditors(self) -> List[str]:
+        """
+        :return: The id's of all the editors stored
+        """
         return list(self.editors.keys())
 
 
 class DocStats:
+    """
+    Class to collate all the stats from each of the calculation phases
+    """
     individuals: IndividualStats
     timeline: TimelineStats
 
-    def __init__(self):
+    def __init__(self, incrementSize):
         self.editors = []
-        self.timeline = TimelineStats()
+        self.timeline = TimelineStats(incrementSize)
         self.individuals = IndividualStats()
