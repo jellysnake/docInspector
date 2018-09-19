72abfa08c64c8c6d6adde3ef64133cb73888ef9f
Darcy Ian Trenfield <ditre3@student.monash.edu>
2018-09-19 16:43:41 +1000

    Delete CollectGeneralStats.py

--- Collectors/CollectGeneralStats.py
+++
@@ -1,7 +0,0 @@
-from DocStats import DocStats
-
-
-def collectGeneralStats(stats: DocStats, service):
-    # Print file name
-    file_meta = service.files().get(fileId=stats.general.id).execute()
-    print(file_meta.get('title'))
