2c3c4907a7f2c0cae5e83ff656fb90d9b6711c89
Quinn Roberts <quinn@jelly-snakes.com>
2018-09-24 13:37:04 +1000

    Clean up CollectTimelineStats
     - Add in docstrings
     - Add in comments to make code readable

--- Collectors/CollectTimelineStats.py
+++ Collectors/CollectTimelineStats.py
@@ -4,20 +4,38 @@ from DocStats import DocStats
 
 
 def collectTimelineStats(stats: DocStats, service, args):
+    """
+    Collects stats for the timeline from the official api.
+    At present this is limited to just the last editor in each major revision in each increment
+
+    :param stats: The stat object to fill up
+    :param service: The service to use to make any calls to the api
+    :param args: The arguments passed into the program
+    """
     days, hours, mins = map(int, args.timeIncrement.split(':'))
     timeSize = (((days * 24) + hours) * 60 + mins) * 60 * 1000
     rev_meta = service.revisions().list(fileId=stats.general.id).execute()
     currentTime = timeToMilli(stats.general.creationDate)
 
     i = 0
+    # Iterate until we run out of revisions
     while i < len(rev_meta['items']):
         increment = stats.timeline.makeIncrement()
-        while i < len(rev_meta['items']) and timeToMilli(rev_meta['items'][i]['modifiedDate']) <= currentTime:
+        # Iterate whilst we are in the current increment
+        # Check to make sure we don't run out of revisions
+        while timeToMilli(rev_meta['items'][i]['modifiedDate']) <= currentTime and i < len(rev_meta['items']):
+            # Add the data from this revision
             increment.editors.add(rev_meta['items'][i]['lastModifyingUserName'])
             i += 1
         currentTime += timeSize
 
 
 def timeToMilli(time):
+    """
+    Converts from a google revision timestamp into milliseconds since the Epoch
+
+    :param time: The timestamp to convert
+    :return: The timestamp in milliseconds
+    """
     return datetime.strptime(time,
-                            "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()
+                             "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()
