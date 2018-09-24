40d34a17516cfe4a15e786be4fbe31350aeff346
honeybadger26 <honeybadger26@gmail.com>
2018-09-24 21:18:06 +1000

    improved timeline

--- OutputStats.py
+++ OutputStats.py
@@ -7,13 +7,13 @@ from DocStats import DocStats
 OUTPUT_DIR = "output"
 
 
-def outputStats(stats: DocStats, args):
+def create_timeline(stats: DocStats, args):
     f_name = stats.general.name
     timeline_stats = stats.timeline
 
-    # time increment
-    ti = datetime.strptime(args.timeIncrement, "%H:%M:%S")
-    ti = timedelta(hours=ti.hour, minutes=ti.minute, seconds=ti.second)
+    # time increment size
+    ti = list(map(int, args.timeIncrement.split(':')))
+    ti = timedelta(days=ti[0], hours=ti[1], minutes=ti[2])
 
     # start date/time to display (either date/time of first increment or creation date)
     if args.dates:      # define start date/time
@@ -42,17 +42,16 @@ def outputStats(stats: DocStats, args):
                     }}
                     table {{
                         border-collapse: collapse;
-                        width: 50%;
                     }}
                     th {{
                         border: 1px solid #dddddd;
                         text-align: left;
-                        padding: 8px;
+                        padding: 12px;
                     }}
                     td {{
                         border: 1px solid #dddddd;
                         text-align: left;
-                        padding: 8px;
+                        padding: 10px;
                     }}
                     tr:nth-child(even){{background-color: #f2f2f2}}
                 </style>
@@ -71,43 +70,49 @@ def outputStats(stats: DocStats, args):
     """)
 
     num_increments = timeline_stats.getNumIncrements()
-
-    prev_date = ""
+    prev_date = ""      # keeps track of whether the dates are repeated
 
     for i in range(num_increments):
         increment = timeline_stats.getIncrement(i)
         adds = increment.additions
         rems = increment.removals
 
-        if len(adds) == len(rems) == 0:     # make sure that None is displayed if there were no changes
-            editors = ["None"]
-            amounts = [0]
-            types = ["None"]
-        else:
-            editors = list(adds.keys()) + list(rems.keys())
-            amounts = list(adds.values()) + list(rems.values())
-            types = ["Addition" for _ in range(len(adds))] + ["Removals" for _ in range(len(rems))]
+        if len(adds) == len(rems) == 0:     # skip if there were no changes
+            dt += ti
+            pass
 
-        if dt.strftime('%d/%m/%Y') != prev_date:        # don't repeat dates
-            date_to_display = dt.strftime('%d/%m/%Y')
-            prev_date = dt.strftime('%d/%m/%Y')
-        else:
-            date_to_display = ""
+        for j, (editor, amount) in enumerate(adds.items()):
+            display_date = ""
+            display_time = ""
+            display_type = ""
 
-        prev_time = ""
-
-        for editor, amount, type in zip(editors, amounts, types):
-            if dt.strftime('%d/%m/%Y') != prev_time:        # don't repeat times
-                prev_time = dt.strftime('%d/%m/%Y')
-                time_to_display = dt.strftime('%I:%M:%S %p')
-            else:
-                time_to_display = ""
+            if j == 0:
+                if prev_date != dt.strftime('%d/%m/%Y'):        # don't repeat dates
+                    display_date = dt.strftime('%d/%m/%Y')
+                    prev_date = dt.strftime('%d/%m/%Y')
+                display_time = dt.strftime('%I:%M:%S %p')
+                display_type = "Additions"
 
             file.write(f"""
                     <tr>
-                        <td>{date_to_display}</td>
-                        <td>{time_to_display}</td>
-                        <td>{type}</td>
+                        <td>{display_date}</td>
+                        <td>{display_time}</td>
+                        <td>{display_type}</td>
+                        <td>{editor}</td>
+                        <td>{amount}</td>
+                    </tr>
+            """)
+
+        for j, (editor, amount) in enumerate(rems.items()):
+            display_type = ""
+            if j == 0:
+                display_type = "Removals"
+
+            file.write(f"""
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>{display_type}</td>
                         <td>{editor}</td>
                         <td>{amount}</td>
                     </tr>
@@ -124,3 +129,7 @@ def outputStats(stats: DocStats, args):
     """)
     file.close()
     open_new(file_path)
+
+
+def outputStats(stats: DocStats, args):
+    create_timeline(stats, args)
