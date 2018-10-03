191e25733f8ff834c01a24bb31e358466994f62c
Quinn Roberts <quinn@jelly-snakes.com>
2018-10-04 00:06:40 +1000

    Convert DocStats#TimelineStats to use the IndividualStats class intenally for each revision

--- DocStats.py
+++ DocStats.py
@@ -12,110 +12,6 @@ class GeneralStats:
         self.id = ""
         self.link = ""
         self.creationDate = ""
-
-
-class TimelineStats:
-    """
-    Collates stats calculated for the timeline
-    """
-
-    class IncrementStats:
-        """
-        Represents a single increment of time
-        """
-
-        def __init__(self):
-            self.additions = {}
-            self.removals = {}
-            self.changes = {}
-            self.editors = set()
-
-        def isEmpty(self):
-            """
-            :return: True if the increment contained no changes. False otherwise
-            """
-            return self.changes
-
-        def addAddition(self, editor, size):
-            """
-            Adds an addition to the increment.
-            Automatically updates the edit counter.
-
-            :param editor: The id of the editor that made the addition
-            :param size: The size of the edit in characters
-            """
-            if editor not in self.additions:
-                self.additions[editor] = 0
-                self.changes[editor] = 0
-
-            self.additions[editor] = size
-            self.changes[editor] += 1
-
-        def addRemoval(self, editor, size):
-            """
-            Adds a removal to the increment.
-            Automatically updates the edit counter.
-
-            :param editor: The editor that made the removal
-            :param size: The size of the edit in characters
-            """
-            if editor not in self.additions:
-                self.removals[editor] = 0
-                self.changes[editor] = 0
-
-            self.removals[editor] = size
-            self.changes[editor] += 1
-
-    def __init__(self, size):
-        self.increments = []
-        self.incrementSize = size
-
-    def getIncrement(self, id) -> IncrementStats:
-        """
-        Get the given increment.
-
-        :param id: The id of the increment to get
-        :return: The increment, if it exists.
-        """
-        return self.increments[id]
-
-    def makeIncrement(self) -> IncrementStats:
-        """
-        Appends a new increment and adds it to the list
-
-        :return: THe newly created increment
-        """
-        self.increments.append(self.IncrementStats())
-        return self.increments[-1]
-
-    def removeIncrement(self, id):
-        """
-        Removes the given increment from the list
-
-        :param id: The increment to remove
-        """
-        self.increments.pop(id)
-
-    def getIncrementSize(self):
-        """
-        :return: The size of each increment
-        """
-        return self.incrementSize
-
-    def setIncrementSize(self, size):
-        """
-        :param size:  the size of each increment
-        """
-        self.incrementSize = size
-
-    def getNumIncrements(self):
-        """
-        :return: how many increments are stored
-        """
-        return len(self.increments)
-
-    def mergeIn(self, other):
-        pass
         self.parent = ref(parent)
 
 
@@ -225,6 +121,62 @@ class IndividualStats:
         for editor in self.editors:
             self.editors[editor].percent = (self.editors[editor].removals + self.editors[editor].additions) \
                                            / (self.total.additions + self.total.removals)
+
+
+class TimelineStats:
+    """
+    Collates stats calculated for the timeline
+    """
+    parent: 'DocStats'
+
+    def __init__(self, size, parent):
+        self.increments = []
+        self.incrementSize = size
+        self.parent = ref(parent)
+
+    def getIncrement(self, id) -> IndividualStats:
+        """
+        Get the given increment.
+
+        :param id: The id of the increment to get
+        :return: The increment, if it exists.
+        """
+        return self.increments[id]
+
+    def makeIncrement(self) -> IndividualStats:
+        """
+        Appends a new increment and adds it to the list
+
+        :return: THe newly created increment
+        """
+        self.increments.append(IndividualStats(self))
+        return self.increments[-1]
+
+    def removeIncrement(self, id):
+        """
+        Removes the given increment from the list
+
+        :param id: The increment to remove
+        """
+        self.increments.pop(id)
+
+    def getIncrementSize(self) -> int:
+        """
+        :return: The size of each increment
+        """
+        return self.incrementSize
+
+    def setIncrementSize(self, size):
+        """
+        :param size:  the size of each increment
+        """
+        self.incrementSize = size
+
+    def getNumIncrements(self) -> int:
+        """
+        :return: how many increments are stored
+        """
+        return len(self.increments)
 
 
 class DocStats:
