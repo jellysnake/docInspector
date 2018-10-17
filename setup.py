62348bf2c607fa694685242a23d8af394debb5f2
Quinn Roberts <quinn@jelly-snakes.com>
2018-10-17 17:58:30 +1100

    Refactor stucture to work better (ie, at all)

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="DocInspector",
-    version="0.1",
+    name='DocInspector',
+    version='0.1.0',
+    author='Quinn Roberts, Simon Schippl, Darcy Trenfield & Ganesh Ukwatt',
+    url='https://git.infotech.monash.edu/FIT2101-S2-2018-Scrumbags',
+    author_email="invalid@invalid.com",
+    license='LICENSE.txt',
+    description='A program made to provide statistical information about google documents and their revisions',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "oauth2client >= 4.1",
+        "google-api-python-client >=1.7",
+        'httplib2'
+    ],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'docInspector=docInspector:main'
+            'DocInspector=DocInspector.docInspector:main'
         ]
     }
 )
